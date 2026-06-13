0024965c9490f0260f9d210a781687281f98ae81
INDavid04 <davidirimia100@gmail.com>
2026-06-13 08:33:27 +0300

    fix: search and add: youtube link for Shalom

--- script.js
+++ script.js
@@ -115,7 +115,7 @@ function playYouTubeVideo(titleElement, youtubeUrl) {
     }
 }
 
-// Funcția To Top
+/// Move the section to the top
 function totop(event) {
     const label = event.currentTarget;
     const section = label.closest('section');
@@ -125,10 +125,43 @@ function totop(event) {
     }
 }
 
+/// Input type search based on find in page CTRL+F shortcut
+function searchText() {
+    const searchInput = document.getElementById('searchInput');
+    const searchText = searchInput.value.trim().toLowerCase();
+    const h2Elements = document.querySelectorAll('section h2'); // Select all h2 elements
+    
+    if (searchText === '') {
+        return;
+    }
+    
+    let found = false;
+    h2Elements.forEach(function(h2) {
+        const text = h2.textContent.trim().toLowerCase();
+        if (text.includes(searchText)) {
+            found = true;
+            const section = h2.closest('section');
+            section.scrollIntoView({ behavior: 'smooth', block: 'start' });
+            return;
+        }
+    });
+    /// if (!found) { alert('No results found'); } /// pretty annoying to give an allert at any small letter mistakes
+}
+
+/// Remove searched input from search bar on Enter
+function clearInput(event) {
+    if (event.key === 'Enter') {
+        const searchInput = document.getElementById('searchInput');
+        searchInput.value = ''; // Clear the value of the input
+    }
+}
+
 // Evenimentele DOM
 document.addEventListener('DOMContentLoaded', function() {
     const addButtons = document.querySelectorAll('.add input[type="checkbox"]');
-    addButtons.forEach(checkbox => checkbox.addEventListener('change', totop));
+    addButtons.forEach(function(checkbox) {
+        checkbox.addEventListener('change', totop);
+    });
 
     const songTitles = document.querySelectorAll('h2[data-youtube]');
     songTitles.forEach(title => {
